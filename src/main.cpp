1b56bb33ffd26b4f03567c022886a977c9f8df72
ehodu <ceduardolopes666@gmail.com>
2024-03-05 20:20:17 -0300

    Commit principal do game

---
+++ src/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+    
+    int TotalBullet, BulletPosition, Shoot, TotalPlayers;
+    bool alive = true;
+    
+    do {
+        
+        cout << "Digite o número de jogadores: ";
+        cin >> TotalPlayers;
+        
+        cout << "Digite o número de balas no canhão: ";
+        cin >> TotalBullet;
+        
+        for(int i=1; i<=TotalPlayers; i++) {
+            cout << "Hora do player " << i << " ..." << endl;
+            cout << "Digite um número entre 1 e " << TotalBullet << " para atirar: ";
+            cin >> Shoot;
+            if(Shoot < 1 || Shoot > 6) {
+                cout << "Opção inválida!" << endl;
+                break;
+            }
+            
+            BulletPosition = rand() % TotalBullet;
+            
+            if(Shoot == BulletPosition) {
+                cout << "Player " << i << " morto!" << endl << endl;
+                alive = false;
+            } else {
+                cout << "UFA! Por pouco!" << endl << endl;
+            }
+        }
+        
+    } while(alive);
+    
+    return 0;
+}
